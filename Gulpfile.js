ceb169ee21f01ea3989f8ad926b2de850db14284
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-06-21 21:54:41 +0300

    03 - إعداد بيئة العمل

---
+++ Gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true,
+    port: 8080
+  })
+});
+
+gulp.task('watch', function() {
+  return gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+  .pipe(gulp.dest('build'))
+  .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+  return gulp.src('src/css/*.css')
+  .pipe(gulp.dest('build/css'))
+  .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/*.js')
+  .pipe(gulp.dest('build/js'))
+  .pipe(connect.reload());
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/*')
+  .pipe(gulp.dest('build/fonts'))
+  .pipe(connect.reload());
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/*')
+  .pipe(gulp.dest('build/images'))
+  .pipe(connect.reload());
+});
+
+gulp.task('build', ['html', 'css', 'js', 'fonts', 'images']);
+
+gulp.task('default', ['connect', 'build', 'watch']);
