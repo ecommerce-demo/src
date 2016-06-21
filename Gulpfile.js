a230240ade84ffdb58ffbdd80b61534b677997bc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-06-21 14:35:57 +0300

    Initial build process

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var flipper = require('gulp-css-flipper');
 var merge = require('merge-stream');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('html', function() {
   return gulp.src('src/*.html')
@@ -11,15 +15,20 @@ gulp.task('html', function() {
 
 gulp.task('css', function() {
   var flip = gulp.src(['src/css/*.css', '!src/css/style.css']).pipe(flipper());
-  var noFlip = gulp.src('src/css/style.css');
+  var noFlip = gulp.src('src/css/style.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }));
   
   return merge(flip, noFlip)
+  .pipe(cleanCSS())
   .pipe(gulp.dest('build/css'))
   .pipe(connect.reload());
 });
 
 gulp.task('js', function() {
   return gulp.src('src/js/*.js')
+  .pipe(uglify())
   .pipe(gulp.dest('build/js'))
   .pipe(connect.reload());
 });
@@ -37,6 +46,7 @@ gulp.task('fonts', function() {
 
 gulp.task('images', function() {
   return gulp.src('src/images/*')
+  .pipe(imagemin())
   .pipe(gulp.dest('build/images'))
   .pipe(connect.reload());
 });
