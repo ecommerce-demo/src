fe1014416e6fe9f26ab5bec269ec6d40ac23c9b9
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-06-22 06:37:59 +0300

    إضافة حوافّ على يسار الزّر

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var flipper = require('gulp-css-flipper');
+var merge = require('merge-stream');
 
 gulp.task('connect', function() {
   connect.server({
@@ -21,8 +22,10 @@ gulp.task('html', function() {
 });
 
 gulp.task('css', function() {
-  return gulp.src('src/css/*.css')
-  .pipe(flipper())
+  var flip = gulp.src(['src/css/*.css', '!src/css/style.css']).pipe(flipper());
+  var noFlip = gulp.src('src/css/style.css');
+  
+  return merge(flip, noFlip)
   .pipe(gulp.dest('build/css'))
   .pipe(connect.reload());
 });
