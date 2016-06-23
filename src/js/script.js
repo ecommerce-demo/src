7be87e6f6384cc15f655e5ce106edac445a7d4ea
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-06-23 21:14:42 +0300

    تفعيل الضّغط على المنتج

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
+  $('[data-product]').click(function() {
+    // انتقل إلى صفحة المنتج
+    window.location = 'product.html';
+  });
 })
