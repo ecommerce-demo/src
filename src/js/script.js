949e93877e5dc088eab77791616691197103323f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-06-26 20:54:22 +0300

    إنشاء عناصر اختيار اللون (1)

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   });
 
   $('.product-option input[type="radio"]').change(function() {
-    $('.product-option').removeClass('active');
+    $(this).parents('.product-option').siblings().removeClass('active');
     $(this).parents('.product-option').addClass('active');
   });
 })
