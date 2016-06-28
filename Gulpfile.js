1599a9029688c5cb2ac626f1b1586751b668b9f3
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-06-29 02:12:52 +0300

    تقليص ملفّات JavaScript وCSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,8 @@ var connect = require('gulp-connect');
 var flipper = require('gulp-css-flipper');
 var merge = require('merge-stream');
 var del = require('del');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
   connect.server({
@@ -31,12 +33,14 @@ gulp.task('css', function() {
   var noFlip = gulp.src('src/css/style.css');
   
   return merge(flip, noFlip)
+  .pipe(cleanCSS())
   .pipe(gulp.dest('build/css'))
   .pipe(connect.reload());
 });
 
 gulp.task('js', function() {
   return gulp.src('src/js/*.js')
+  .pipe(uglify())
   .pipe(gulp.dest('build/js'))
   .pipe(connect.reload());
 });
